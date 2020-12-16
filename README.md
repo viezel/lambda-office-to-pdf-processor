b44a437cd85037d4f3ed4170ce2282755d4c76f0
Mads Møller <info@viezel.dk>
2020-12-16 15:20:24 +0100

    init

---
+++ README.md
@@ -0,0 +1,56 @@
+# File Processor
+
+Convert Office (docx, pptx, xlsx) to PDF at scale. 
+
+Support for **custom fonts** to conversion. See the `fonts` folder for supported fonts. Add your own to support even more.
+
+## Dependencies
+
+* Serverless Framework
+* AWS Lambda
+* LibreOffice Lambda Layer (https://github.com/shelfio/libreoffice-lambda-layer)
+
+
+## Config
+
+Uses the `serverless-config.dev.json` or the `serverless-config.prod.json` to handle different AWS resources, such as buckets, SQS etc.
+
+Uses SSM Parameter Store (console.aws.amazon.com/systems-manager/parameters) to keep your API-Key safe. Visit the AWS console to setup your parameters.
+
+You have to create `FileProcessor/dev/ApiKey`
+
+## Usage
+
+Once deployed, you can hit the api like this:
+
+```bash
+curl --location 
+--request POST 'https://someurl.execute-api.eu-west-1.amazonaws.com/dev/api/v1/file/convert' \
+--header 'X-API-KEY: my-custom-api-key' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    "url": "https://my-website.com/path/to/doc.pptx"
+}'
+```
+
+Response:
+
+```json
+{
+    "output_url": "https://office-file-processor-dev.s3.eu-west-1.amazonaws.com/conversions/1608127441661/doc.pdf"
+}
+```
+
+## DEPLOYMENT
+
+### Deploy Dev
+
+```bash
+serverless deploy --stage dev --verbose
+```
+
+### Deploy Production
+
+```bash
+serverless deploy --stage prod --verbose
+```
---
+++ fileconvert.js
@@ -0,0 +1,112 @@
+'use strict';
+
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
+const https = require('https');
+const http = require('http');
+const aws = require('aws-sdk');
+const s3 = new aws.S3();
+
+const { convertTo } = require('@shelf/aws-lambda-libreoffice');
+const API_KEY = 'X-API-KEY';
+
+function downloadFile(url, filePath) {
+    const proto = !url.charAt(4).localeCompare('s') ? https : http;
+    return new Promise((resolve, reject) => {
+        const file = fs.createWriteStream(filePath);
+        let fileInfo = null;
+        const request = proto.get(url, response => {
+            if (response.statusCode !== 200) {
+                reject(new Error(`Failed to get '${url}' (${response.statusCode})`));
+                return;
+            }
+            fileInfo = {
+                mime: response.headers['content-type'],
+                size: parseInt(response.headers['content-length'], 10),
+            };
+            response.pipe(file);
+        });
+        file.on('finish', () => resolve(fileInfo));
+        request.on('error', err => {
+            fs.unlink(filePath, () => reject(err));
+        });
+        file.on('error', err => {
+            fs.unlink(filePath, () => reject(err));
+        });
+        request.end();
+    });
+}
+
+/**
+ * HTTP Handler from URL
+ * @param event
+ * @param context
+ * @param callback
+ * @returns {Object} object - API Gateway Lambda Proxy Output Format
+ */
+exports.httpHandlerFromURL = function (event, context, callback) {
+    // Validate API Auth
+    if( undefined === event.headers[API_KEY] || event.headers[API_KEY] !== process.env.API_KEY) {
+        return callback(null, {
+            statusCode: 401,
+            body: JSON.stringify({error:'Authentication credentials were missing or incorrect'})
+        });
+    }
+
+    /*
+        // example http payload
+        {
+            "url": "https://example.com/my/file.pptx"
+        }
+     */
+
+    // parse http payload
+    let data = JSON.parse(event.body);
+    const url = data.url,
+        file = url.split('/').pop(),
+        bucket = process.env.BUCKET,
+        workdir = os.tmpdir(),
+        inputFile = path.join(workdir, file),
+        outputFile = inputFile.replace(/\.[^.]+$/, '.pdf'),
+        outputKey = 'conversions/' + Math.round(new Date().getTime()).toString() + "/" + file.replace(/\.[^.]+$/, '.pdf');
+
+    // Validate http payload
+    if (!url || !file) {
+        return callback(null, { statusCode: 422, body: JSON.stringify({error:'Validation error'}) });
+    }
+
+    // 1. download file from URL
+    downloadFile(url, inputFile)
+        // 2. convert to PDF
+        .then(() => convertTo(file, 'pdf'))
+        // 3. upload converted file to S3
+        .then(() => {
+            return s3.upload({
+                Bucket: bucket,
+                Key: outputKey,
+                Body: fs.createReadStream(outputFile),
+                ACL: 'public-read',
+                CacheControl: "max-age=314496000,immutable",
+                ContentType: 'application/pdf'
+            }).promise();
+        })
+        // 4. create a http response
+        .then(function (data) {
+            context.succeed({
+                statusCode: 200,
+                body: JSON.stringify({
+                    output_url: data.Location
+                }),
+                headers: {'Content-Type': 'application/json'}
+            });
+        })
+        .catch(function(error) {
+            console.error(error);
+            context.fail({
+                statusCode: 500,
+                body: JSON.stringify({ error: error}),
+                headers: {'Content-Type': 'application/json'}
+            });
+        });
+};
